3e7cc27839080b0b93d47a00c05481cb620c88cc
Victor Paro <victorparo@hotmail.com>
2021-07-05 13:32:46 -0300

    Creating src directory and moving utils inside

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { openTorrent } from './utils/torrent-handler.js';
-import { getPeers } from './utils/tracker.js';
+import { openTorrent } from './src/utils/torrent-handler.js';
+import { getPeers } from './src/utils/tracker.js';
 
 const torrent = openTorrent('endeavouros.iso.torrent'));
 getPeers(torrent, peers => {
